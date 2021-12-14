275c3c8af3e03a4dc6d533baf3bacc28c7af08b0
Alan <alanrd772@gmail.com>
2021-12-14 22:32:56 +0400

    fix on unsupported browsers

--- js/404.js
+++ js/404.js
@@ -1,6 +1,6 @@
 // Haptics to click while the 404 page shakes
 $(document).ready(() => {
-    navigator.vibrate([5,95,5,95,3,97,2,98,1])
+    vibrate()
 })
 
 div = $(".container.shake")
@@ -8,12 +8,16 @@ div.click(() => {
     div.removeClass("shake")
     window.requestAnimationFrame(function() {
         div.addClass("shake")
-        // on Firefox Mobile or other unsupported browsers this not working
-        // should hopefully not break the animation repeat function
-        try {
-            navigator.vibrate([5,100,5,100,3,100,2,100,1])
-        } catch(e) {
-            console.log(`Vibration likely unsupported\n${e}`)
-        }
+        vibrate()
     });
 })
+
+vibrate = () => {
+    // on Firefox Mobile or other unsupported browsers this not working
+    // should hopefully not break the animation repeat function
+    try {
+        navigator.vibrate([5,100,5,100,3,100,2,100,1])
+    } catch(e) {
+        console.log(`Vibration likely unsupported\n${e}`)
+    }
+}
