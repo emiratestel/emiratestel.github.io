9d7ca499888a315dbd876c6ce6b83a547e9f0094
Alan <alanrd772@gmail.com>
2021-12-14 17:31:06 +0400

    fixed haptics

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Haptics when link is pressed or released
-$("button a").mousedown(() => {
+$("button, a").mousedown(() => {
+    console.log("h")
     navigator.vibrate(5)
 }).mouseup(() => {
+    console.log("h")
     navigator.vibrate(5)
 })
