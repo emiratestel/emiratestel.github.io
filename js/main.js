b95e2decbf885b5e883828d75a2ccfe7416ac74f
Alan <alanrd772@gmail.com>
2021-12-20 14:00:21 +0400

    mousedown&up changed since they would fire (?) on mobile causing a duplicate trigger

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Haptics when link is pressed or released
 $("button, a", true).forEach(e => {
     // for each element in the array, add an event listener of the same name
-    ["mousedown","mouseup","touchend"].forEach(event => {
-        e.addEventListener(event, () => { navigator.vibrate(3) })
+    ["click","touchend"].forEach(event => {
+        e.addEventListener(event, () => { navigator.vibrate(5) })
     })
 })
