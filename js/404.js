dcb035cdf59430eca9351e298127e58059ee885e
Alan <alanrd772@gmail.com>
2021-12-20 14:02:33 +0400

    Remove jQuery from 404.js

--- js/404.js
+++ js/404.js
@@ -1,21 +1,21 @@
 // Haptics to click while the 404 page shakes
-$(document).ready(() => {
+ready(() => {
     vibrate()
 })
 
 div = $(".container.shake")
-div.click(() => {
-    div.removeClass("shake")
+["click","touchend"].forEach(event => {
+    div.addEventListener(event, () => {
+        div.removeClass("shake")
 
-    // needs a delay because otherwise Firefox doesn't repeat the animation
-    window.setTimeout(() => {
-        window.requestAnimationFrame(() => {
-            div.addClass("shake")
-            vibrate()
-        });
-    },1)
+        // needs a delay because otherwise Firefox doesn't repeat the animation
+        window.setTimeout(() => {
+            window.requestAnimationFrame(() => {
+                div.addClass("shake")
+                vibrate()
+            });
+        },1)
+    })
 })
 
-vibrate = () => {
-    navigator.vibrate([5,100,5,100,3,100,2,100,1])
-}
+vibrate = () => { navigator.vibrate([5,100,5,100,3,100,2,100,1]) }
