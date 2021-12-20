c8133124ff77e9701db220d390ed7baa132721af
Alan <alanrd772@gmail.com>
2021-12-20 13:52:42 +0400

    Touchstart event removed as it is delayed in some browsers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Haptics when link is pressed or released
 $("button, a", true).forEach(e => {
     // for each element in the array, add an event listener of the same name
-    ["mousedown","touchstart","mouseup","touchend"].forEach(event => {
+    ["mousedown","mouseup","touchend"].forEach(event => {
         e.addEventListener(event, () => { navigator.vibrate(3) })
     })
 })
