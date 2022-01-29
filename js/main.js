579a819c276bcaf7ea1c4253cb4a188e32d4eea0
Alan <alanrd772@gmail.com>
2022-01-29 15:31:02 +0400

    Donation box

--- js/main.js
+++ js/main.js
@@ -5,3 +5,28 @@ $("button, a", true).forEach(e => {
         e.addEventListener(event, () => { navigator.vibrate(5) });
     });
 });
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (i in ca) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return;
+}
+donateModal = $(".toast.donate")
+if (getCookie("donateclosed") !== "true")
+    donateModal.classList.add("show")
+$(".toast.donate .btn.close").addEventListener("click", () => {
+    const date = new Date();
+    date.setTime(date.getTime() + (2*24*60*60*1000));
+    document.cookie = `donateclosed=true; expires=${date.toUTCString()}; path=/`;
+})
